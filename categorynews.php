637c6694e36cd546dd4c8c7542ada8a048cbc668
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-01-28 11:01:24 +0545

    view news category wise

---
+++ categorynews.php
@@ -0,0 +1,33 @@
+<?php include('includes/header.php') ;
+$catid = $_GET['catid'];
+$qrycat = "SELECT * FROM categories WHERE id=$catid";
+$qry = "SELECT * FROM news WHERE category_id=$catid ORDER BY news_date desc";
+include 'includes/dbconnection.php';
+$catresult = mysqli_query($con,$qrycat);
+$category = mysqli_fetch_assoc($catresult);
+$newsresult = mysqli_query($con,$qry);
+include 'includes/closeconnection.php';
+?>
+<div class="py-10">
+    <h2 class="text-3xl font-bold text-center"><?php echo $category['categoryname'] ?> News</h2>
+
+    <div class="grid grid-cols-4 gap-10 px-24 my-5">
+        <?php 
+        while($news = mysqli_fetch_assoc($newsresult))
+        { ?>
+        <div class="bg-gray-300 rounded-lg">
+            <img src="admin/uploads/<?php echo $news['photopath'];?>" alt="" class="rounded-t-lg h-60 object-cover">
+            <div class="p-2">
+                <h1 class="font-bold text-lg"><?php echo $news['title'];?></h1>
+                <p class="text-gray-600 text-justify line-clamp-3"><?php echo $news['description'];?></p>
+                <div class="text-right text-sm text-red-500">
+                <i class="ri-calendar-line"></i> <?php echo $news['news_date']; ?>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+    
+</div>
+
+<?php include('includes/footer.php') ?>
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,15 @@
+<?php 
+include 'includes/dbconnection.php';
+$qry = "SELECT * FROM categories ORDER BY priority";
+$resultcat = mysqli_query($con,$qry);
+include 'includes/closeconnection.php';
+?>
 <html>
 
 <head>
     <title>My PHP Project</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <link rel="stylesheet" href="tailwind.css">
     <link
     href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"
     rel="stylesheet"
@@ -22,9 +29,12 @@
     <nav class="flex justify-between items-center shadow px-14">
         <img src="https://www.bitmapitsolution.com/images/logo/logo.png" class="h-20" alt="">
         <div>
-            <a class="mx-2" href="">Home</a>
-            <a class="mx-2" href="">About</a>
-            <a class="mx-2" href="">Contact</a>
+            <a class="mx-2" href="index.php">Home</a>
+            <?php while($cat = mysqli_fetch_assoc($resultcat))
+            {
+                ?>
+            <a class="mx-2" href="categorynews.php?catid=<?php echo $cat['id'] ?>"><?php echo $cat['categoryname'] ?></a>
+                <?php } ?>
             <a class="mx-2" href="admin/dashboard.php">Login</a>
         </div>
     </nav>
